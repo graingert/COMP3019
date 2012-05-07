aca5a3ea2c55458f3455c6851449fd555e57bedc
graingert <tagrain@gmail.com>
2012-05-07 23:05:05 +0100

    Added applet and dependency on mgridclient

---
+++ mgrid-wordcount/src/uk/co/graingert/mgrid/lettercount/LetterCountingMGridApplet.java
@@ -0,0 +1,12 @@
+package uk.co.graingert.mgrid.lettercount;
+
+import mgridapplet.MGridApplet;
+public class LetterCountingMGridApplet extends MGridApplet{
+
+	@Override
+	public void initApplet(int parameterNum, String parameter) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
