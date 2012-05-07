3fad9e4273abd0025fd89b838404c23f273d7fe6
graingert <tagrain@gmail.com>
2012-05-07 23:54:54 +0100

    Filled method stub, now counts letters given by parameter

--- mgrid-wordcount/src/uk/co/graingert/mgrid/lettercount/LetterCountingMGridApplet.java
+++ mgrid-wordcount/src/uk/co/graingert/mgrid/lettercount/LetterCountingMGridApplet.java
@@ -1,12 +1,28 @@
 package uk.co.graingert.mgrid.lettercount;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+
 import mgridapplet.MGridApplet;
 public class LetterCountingMGridApplet extends MGridApplet{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6119217355265008276L;
+
 	@Override
 	public void initApplet(int parameterNum, String parameter) {
-		// TODO Auto-generated method stub
-		
+		InputStream in = getClass().getResourceAsStream("textfile.txt");
+		try {
+			String textfile = IOUtils.toString(in, "UTF-8");
+			int count = StringUtils.countMatches(textfile, parameter);
+			submitResults(Integer.toString(count));
+		} catch (IOException e) {
+			submitException(e);
+		}
 	}
-
 }
